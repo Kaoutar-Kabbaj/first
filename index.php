744aad33ed534aac6bd0b2f9c9c132e0df078fe5
user-PC\user <k.kabbaj@mundiapolis.ma>
2017-01-08 13:23:40 +0000

    commit

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
 
-echo 'Hello Abdel'; 
 
+
+
+class DBManager{
+function getConnection(){
+$services = getenv("VCAP_SERVICES");
+$services_json = json_decode($services,true);
+$mysql_config = $services_json["mysql"][0]["credentials"];
+var_dump($mysql_config);die;s
+$db = $mysql_config["name"];
+$host = $mysql_config["host"];
+$port = $mysql_config["port"];
+$username = $mysql_config["user"];
+$password = $mysql_config["password"];
+$conn = mysql_connect($host . ':' . $port, $username, $password);
+if(! $conn ){
+die('Could not connect: ' . mysql_error());
+}
+mysql_select_db($db);
+return $conn;
+}
+
+
+}
+
+$manager = new DBManager();
+$manager->getConnection();
 ?>
 
-
-<?php
-$link = mysql_connect("198.11.234.66", "u6n85qutOuBnx", "pMRPPWodbZ9BE")
-    or die("Impossible de se connecter : " . mysql_error());
-echo 'Connexion réussie';
-mysql_close($link);
-?>
