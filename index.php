af18bb73340c510226a5eae0d8bc411ea2249e06
user-PC\user <k.kabbaj@mundiapolis.ma>
2017-01-08 12:49:15 +0000

    commit

--- index.php
+++ index.php
@@ -3,3 +3,11 @@
 echo 'Hello Abdel'; 
 
 ?>
+
+
+<?php
+$link = mysql_connect("198.11.234.66", "u6n85qutOuBnx", "pMRPPWodbZ9BE")
+    or die("Impossible de se connecter : " . mysql_error());
+echo 'Connexion réussie';
+mysql_close($link);
+?>
